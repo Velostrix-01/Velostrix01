642e6ca621115ef29cde0e6537fb2034cc8a4085
Velostrix <velostrix@gmail.com>
2026-01-27 00:33:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function toggleSubject(id) {
   const el = document.getElementById(id);
   el.style.display = el.style.display === "block" ? "none" : "block";
 }
+function toggleMenu() {
+  document.getElementById("nav-menu").classList.toggle("show");
+}
