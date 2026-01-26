d4942527dec0cb8a83e6641dcf1e62c7eb7f0510
Velostrix <velostrix@gmail.com>
2026-01-27 00:47:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-function toggleSubject(id) {
-  const el = document.getElementById(id);
-  el.style.display = el.style.display === "block" ? "none" : "block";
+function toggleSubject(id, el) {
+  const content = document.getElementById(id);
+  const parent = el.closest(".subject");
+
+  if (content.style.display === "block") {
+    content.style.display = "none";
+    parent.classList.remove("active");
+  } else {
+    content.style.display = "block";
+    parent.classList.add("active");
+  }
 }
+
 function toggleMenu() {
   document.getElementById("nav-menu").classList.toggle("show");
 }
+
