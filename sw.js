c57adf4e585bd580cc0d30e0f8a8e7f1d201b8d5
Velostrix <velostrix@gmail.com>
2026-02-07 17:08:38 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "velostrix-cache-v1";
+const OFFLINE_URL = "offline.html";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        OFFLINE_URL,
+        "/",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match(OFFLINE_URL);
+      })
+    );
+  }
+});
