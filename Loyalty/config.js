4fe8b005127415aba456a27c2acb6e2283b331e3
Velostrix <velostrix@gmail.com>
2026-09-26 13:54:16 +0530

    Update config.js

--- Loyalty/config.js
+++ Loyalty/config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://nfzhrckgubuxyrodftxg.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_XUobTJ-ynbXQDHWg12A5kw_Z0-ioxmq",
+  SUPABASE_URL: "https://zrjgjfkafpcfnxdenxci.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_EnyT8UBI1FPqcpWNOnu3tw_mEmj0c4P",
   CITY: "Sambhaji Nagar (Aurangabad)",
   CITY_SHORT: "Sambhaji Nagar",
-  SUPPORT_PHONE: ""
+  SUPPORT_PHONE: "+91 7888088346"
 };
